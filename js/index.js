69d6a1bb27253d8bb958910c96acdb70a33ca537
Alfred <ya95123@gmail.com>
2021-08-05 17:29:41 +0800

    circle mousemove dunction & todo

--- js/index.js
+++ js/index.js
@@ -1,15 +1,35 @@
 const circleBgs = document.querySelectorAll(".circle-bg")
 const circles = document.querySelectorAll(".circle")
 
-// *circleBG 範圍內被 mousedown
+// *拖拉同心圓 function
 circleBgs.forEach((item, idx) => {
+  // 監聽滑鼠移動開關
+  let move
+
+  // *circleBG 範圍內被 mousedown
   item.addEventListener("mousedown", (e) => {
-    console.log(e.pageX);
+    // 紀錄 dom 寬、高
+    let circleWidth = circles[0].offsetWidth
+    let circleHeight = circles[0].offsetHeight
+    // 紀錄 mouse 一開始 X 起點
+    let mouseX = e.pageX
+
+    // 可以移動
+    move = true
 
     // *觸發滑鼠移動監聽 window mousemove
     window.addEventListener("mousemove", (e) => {
-      console.log(e.pageY);
+      if (move) {
+        console.log(e.pageX - mouseX);
+        // TODO circle 大小改變，要記得加上絕對值！
+        let percent = e.pageX - mouseX
+        circles[idx].setAttribute("style", `width:${circleWidth + percent}%; height:${circleHeight + percent}%;`)
+      }
     })
+
     // *關閉滑鼠移動監聽 window mouseup
+    window.addEventListener("mouseup", () => {
+      move = false
+    })
   })
 })
