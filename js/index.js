db7d801dc0fa3431c13c0fbb1e0c1f526d6d3173
Alfred <ya95123@gmail.com>
2021-08-09 12:07:44 +0800

    circle drag &change size

--- js/index.js
+++ js/index.js
@@ -3,33 +3,31 @@ const circles = document.querySelectorAll(".circle")
 
 // *拖拉同心圓 function
 circleBgs.forEach((item, idx) => {
-  // 監聽滑鼠移動開關
-  let move
 
   // *circleBG 範圍內被 mousedown
   item.addEventListener("mousedown", (e) => {
-    // 紀錄 dom 寬、高
-    let circleWidth = circles[0].offsetWidth
-    let circleHeight = circles[0].offsetHeight
-    // 紀錄 mouse 一開始 X 起點
-    let mouseX = e.pageX
+    e.preventDefault()
 
-    // 可以移動
-    move = true
+    // 紀錄 dom 中心 X 座標 (offetLeft 是與父元素的左側距離)
+    let circleBgCenter = item.offsetLeft + (item.offsetWidth / 2)
+    console.log(circleBgCenter)
+
+    // circle 大小改變，加上絕對值！
+    const moveCircle = (e) => {
+      // e.pageX　為滑鼠 X 座標
+      let percent = Math.abs(e.pageX - circleBgCenter)
+      console.log(percent)
+      // 更改 circle 寬高 %
+      circles[idx].setAttribute("style", `width:${percent}%; height:${percent}%;`)
+    }
 
     // *觸發滑鼠移動監聽 window mousemove
-    window.addEventListener("mousemove", (e) => {
-      if (move) {
-        console.log(e.pageX - mouseX);
-        // TODO circle 大小改變，要記得加上絕對值！
-        let percent = e.pageX - mouseX
-        circles[idx].setAttribute("style", `width:${circleWidth + percent}%; height:${circleHeight + percent}%;`)
-      }
-    })
+    window.addEventListener("mousemove", moveCircle, false)
 
     // *關閉滑鼠移動監聽 window mouseup
     window.addEventListener("mouseup", () => {
-      move = false
+      window.removeEventListener("mousemove", moveCircle);
+      console.log("over")
     })
   })
 })
